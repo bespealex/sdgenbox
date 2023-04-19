0bd3feca8a23c0ee9113e6ca8ff51d077640e217
Alexander Bespalov <discrimy.off@gmail.com>
2023-04-19 22:57:41 +0300

    Add trial db support

--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -1,9 +1,27 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{get, web::Data, HttpResponse, Responder};
+use serde_json::json;
+use sqlx::{Pool, Sqlite};
 use tera::Context;
 
-use crate::utils::render::render_html;
+use crate::utils::{errors::MapErrToInternal, render::render_html};
 
 #[get("/")]
 async fn index() -> actix_web::Result<HttpResponse> {
     render_html("index.html", &Context::new(), HttpResponse::Ok())
+}
+
+#[get("/db-test")]
+async fn db_test(pool: Data<Pool<Sqlite>>) -> actix_web::Result<impl Responder> {
+    let mut conn = pool.acquire().await.map_err_to_internal()?;
+
+    let result = sqlx::query!("select id from (select $1+0 as id)", 16)
+        .fetch_one(&mut conn)
+        .await
+        .map_err_to_internal()?;
+
+    render_html(
+        "db_test.html",
+        &Context::from_value(json!({ "result": result.id })).map_err_to_internal()?,
+        HttpResponse::Ok(),
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
 
 mod handlers;
 mod models;
 mod templates;
 mod utils;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let host = "localhost";
     let port = 8080;
 
     env_logger::init();
 
-    let app = HttpServer::new(|| {
+    let connection = sqlx::sqlite::SqlitePoolOptions::new()
+        .connect(":memory:")
+        .await?;
+
+    let app = HttpServer::new(move || {
         App::new()
             .service(handlers::index::index)
+            .service(handlers::index::db_test)
             .service(handlers::upload::upload_get)
             .service(handlers::upload::upload_post)
+            .app_data(Data::new(connection.clone()))
     })
     .bind((host, port))?;
     log::info!("Running server on http://{}:{}/", host, port);
-    app.run().await
+    app.run().await?;
+    Ok(())
 }
